af4a84939ca28a3a6ad26f9335662322c9c49193
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-15 13:05:54 -0500

    Update python_loops_datasets.py
    using   append() and sum ()

--- python_loops_datasets.py
+++ python_loops_datasets.py
@@ -1,19 +1,12 @@
-from csv import reader
-
 opened_file = open('AppleStore.csv')
-
-
+from csv import reader
 read_file = reader(opened_file)
 apps_data = list(read_file)
 
-print(len(apps_data))
-
-
-rating_sum = 0
-
-for  row  in apps_data[1:] : 
+rating_sum = []
+for row in  apps_data[1:]:
     rating = float(row[7])
-    rating_sum+=rating
+    rating_sum.append(rating)
     
-avg_rating = rating_sum / (len(apps_data)-1)
-    
+avg_rating = sum(rating_sum) / (len(apps_data)-1)
+print(avg_rating)
