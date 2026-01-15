b86526f9b814b3142d518e4a710ff7cfa58183d1
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-15 13:09:57 -0500

    Update python_loops_datasets.py

--- python_loops_datasets.py
+++ python_loops_datasets.py
@@ -3,10 +3,9 @@ from csv import reader
 read_file = reader(opened_file)
 apps_data = list(read_file)
 
-rating_sum = []
-for row in  apps_data[1:]:
-    rating = float(row[7])
-    rating_sum.append(rating)
+apps_names = []
+for row in apps_data[1:]:
+    name = row[1]
+    apps_names.append(name)
     
-avg_rating = sum(rating_sum) / (len(apps_data)-1)
-print(avg_rating)
+print(apps_names[:5])
