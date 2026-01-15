992475f075085aa258980f361f01208497d14772
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-15 15:57:49 -0500

    The else Clause
     createnig  a columns

--- python_loops_datasets.py
+++ python_loops_datasets.py
@@ -2,21 +2,15 @@ opened_file = open('AppleStore.csv')
 from csv import reader
 read_file = reader(opened_file)
 apps_data = list(read_file)
-apps_greaterThen_9 = []
 
-
-greaterThen = 0
-less_orEuq = 0
-for i in apps_data[1:]:
-    rating = float(i[7])
+apps_data[0].append('free_or_not')
+for app in apps_data[1:]:
+    price = float(app[4])
+    if price:
+        app.append("not free")
+    else:
+        app.append("free")
     
-    if float(i[4]) > 9:
-        apps_greaterThen_9.append(rating)
-        greaterThen+=1
-    else :
-        less_orEuq+=1
-avg_rating = sum(apps_greaterThen_9) / greaterThen  
 
-n_apps_more_9 = greaterThen
-n_apps_less_9 = less_orEuq
-
+print(apps_data[:6])
+    # Complete code from here
