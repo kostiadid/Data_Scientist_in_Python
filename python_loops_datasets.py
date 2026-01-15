295115db747b8123c89985de6d845a140b377e6f
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-15 16:20:13 -0500

    elif
    elif    and   column apps_data[0].append('price_label')

--- python_loops_datasets.py
+++ python_loops_datasets.py
@@ -3,14 +3,17 @@ from csv import reader
 read_file = reader(opened_file)
 apps_data = list(read_file)
 
-apps_data[0].append('free_or_not')
+
+apps_data[0].append('price_label')
+
 for app in apps_data[1:]:
     price = float(app[4])
-    if price:
-        app.append("not free")
-    else:
-        app.append("free")
+    if price == 0:
+        app.append('free')
+    if price >0 and  price < 20:
+        app.append('affordable')
+    if price >= 20 and price < 50:
+        app.append('expensive')
+    elif price >= 50:
+        app.append("very expensive")
     
-
-print(apps_data[:6])
-    # Complete code from here
