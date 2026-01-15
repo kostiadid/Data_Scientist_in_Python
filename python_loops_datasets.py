67e0c61dee9f009fd01d946125af863e004bdd60
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-15 15:43:12 -0500

    Comparison Operator Applications

--- python_loops_datasets.py
+++ python_loops_datasets.py
@@ -2,12 +2,21 @@ opened_file = open('AppleStore.csv')
 from csv import reader
 read_file = reader(opened_file)
 apps_data = list(read_file)
+apps_greaterThen_9 = []
 
-non_games_ratings = []
 
-for row in apps_data[1:]:
-    rating = float(row[7])
-    genre = row[11]
-    if   genre != 'Games':
-        non_games_ratings.append(rating)
-avg_rating_non_games = sum(non_games_ratings) / len(non_games_ratings)
+greaterThen = 0
+less_orEuq = 0
+for i in apps_data[1:]:
+    rating = float(i[7])
+    
+    if float(i[4]) > 9:
+        apps_greaterThen_9.append(rating)
+        greaterThen+=1
+    else :
+        less_orEuq+=1
+avg_rating = sum(apps_greaterThen_9) / greaterThen  
+
+n_apps_more_9 = greaterThen
+n_apps_less_9 = less_orEuq
+
