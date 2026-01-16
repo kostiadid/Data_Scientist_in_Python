56019808dd751b2e0bd356324171eebc30de1375
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-16 16:16:04 -0500

    dictionary
    dictionary with loops  and counters

--- dictionaris_with_datasets.py
+++ dictionaris_with_datasets.py
@@ -1,1 +1,16 @@
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
 
+content_ratings = {}
+
+for c  in  apps_data[1:]:
+    c_rating = c[10]
+    if c_rating in  content_ratings:
+        content_ratings[c_rating]+=1
+    else : 
+        content_ratings[c_rating]=1
+        print(content_ratings)
+    
+print(content_ratings)
