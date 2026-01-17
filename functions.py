f06136905823d69538d93bd93fe1fccfd780f323
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-17 16:28:03 -0500

    extract  and  freq_table
    simple  manipulations  with data

--- functions.py
+++ functions.py
@@ -1,9 +1,25 @@
+# CODE FROM THE PREVIOUS SCREEN
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
 
-def date(month, day, year):
-    return month + " " + str(day)+"," + " "+str(year)
+def extract(index):
+    column = []    
+    for row in apps_data[1:]:
+        value = row[index]
+        column.append(value)    
+    return column
+
+genres = extract(11)
 
 
-
-print(date('July', '15', '2006'))
-print(date('February', '4', '2004'))
-print(date('June', '9', '1949'))
+def freq_table(some_list):
+    dictionary = {}
+    for i in some_list:
+        if i in  dictionary:
+            dictionary[i]+=1
+        else:
+            dictionary[i]=1
+    return dictionary
+genres_ft=freq_table(genres)
