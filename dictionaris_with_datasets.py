649eb616c5747c29d07bc2fc18c399ed7ca44291
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-17 15:37:07 -0500

    Filtering for the Intervals
    Python Dictionaries and Frequency Tables

--- dictionaris_with_datasets.py
+++ dictionaris_with_datasets.py
@@ -4,10 +4,33 @@ read_file = reader(opened_file)
 apps_data = list(read_file)
 
 
-data_sizes = []
+rating_list = []
 for i in  apps_data[1:]:
-    size = float(i[2])
-    data_sizes.append(size)
+    rating_list.append(float(i[5]))
     
-min_size = min(data_sizes)
-max_size = max(data_sizes)
+max_val = max(rating_list)
+min_val = min(rating_list)
+avg_value = sum(rating_list) / len(rating_list)
+intervals = {
+    'min': 0,
+    'before': 0,
+    'avrg': 0,
+    'after': 0,
+    'max+': 0
+}
+for i in apps_data[1:]:
+    val = float(i[5])
+    if val <= 10000:
+        intervals['min'] += 1
+    elif 10000 < val <= 100000:
+        intervals['before'] += 1
+    elif 100000 < val <= 500000:
+        intervals['avrg'] += 1
+    elif 500000 < val <= 1000000:
+        intervals['after'] += 1
+    elif val > 1000000:
+        intervals['max+'] += 1
+        
+print(intervals)
+        
+
