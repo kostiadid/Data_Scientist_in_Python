b4781914f31c75e958386bfc563cf530ab283812
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-17 16:56:38 -0500

    Positional or named parameters

--- functions.py
+++ functions.py
@@ -3,24 +3,19 @@ from csv import reader
 read_file = reader(opened_file)
 apps_data = list(read_file)
 
-
-def freq_table(column_num,data):
-    column_list = []
-    freq_dict = {}
-    for i in data[1:]:
-        column_list.append(i[column_num])
-    for i in  column_list:
-        if i in  freq_dict:
-            freq_dict[i]+=1
+def freq_table(data_set, index):
+    frequency_table = {}
+    
+    for row in data_set[1:]:
+        value = row[index]
+        if value in frequency_table:
+            frequency_table[value] += 1
         else:
-            freq_dict[i]=1
-    return  freq_dict
-    
-    
-ratings_ft=freq_table(7,apps_data)    
-    
+            frequency_table[value] = 1
         
-        
-        
-        
-    
+    return frequency_table
+
+
+content_ratings_ft = freq_table(apps_data , 10)
+ratings_ft = freq_table(data_set = apps_data , index = 7)
+genres_ft = freq_table(data_set = apps_data , index = 11)
