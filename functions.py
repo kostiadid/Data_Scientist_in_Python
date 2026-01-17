877193658232878c8b7d59ca773bf8d84357f227
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-17 15:58:36 -0500

    functoin exp

--- functions.py
+++ functions.py
@@ -1,1 +1,9 @@
 
+def date(month, day, year):
+    return month + " " + str(day)+"," + " "+str(year)
+
+
+
+print(date('July', '15', '2006'))
+print(date('February', '4', '2004'))
+print(date('June', '9', '1949'))
