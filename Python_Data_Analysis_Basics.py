9bfa026922d407f11480692a29245827aa97f048
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-19 21:27:20 -0500

    Python Data Analysis Basics

--- Python_Data_Analysis_Basics.py
+++ Python_Data_Analysis_Basics.py
@@ -1,1 +1,7 @@
 
+# Working with the dataset, we'll learn how to do the following:      //'artworks_clean.csv'
+
+# Calculate the artist's age when they created their artwork
+# Analyze and interpret the distribution of artist ages
+# Create functions that summarize our data
+# Print summaries in an easy-to-read-way
