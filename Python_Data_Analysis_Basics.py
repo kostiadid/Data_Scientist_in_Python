a14c72551474c0ca4d47fcb139c0bf17f8382f46
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-19 21:37:29 -0500

    Calculating Artist Ages

--- Python_Data_Analysis_Basics.py
+++ Python_Data_Analysis_Basics.py
@@ -1,39 +1,16 @@
-
-# Working with the dataset, we'll learn how to do the following:      //'artworks_clean.csv'
-
-# Calculate the artist's age when they created their artwork
-# Analyze and interpret the distribution of artist ages
-# Create functions that summarize our data
-# Print summaries in an easy-to-read-way
-
-
-from csv import reader
-
-# Read the `artworks_clean.csv` file
-with open('artworks_clean.csv') as opened_file:
-    read_file = reader(opened_file)
-    moma = list(read_file)
-moma = moma[1:]
-
-# Convert the birthdate values
-for row in moma:
-    birth_date = row[3]
-    if birth_date != "":
-        birth_date = int(birth_date)
-    row[3] = birth_date
-    
-# Convert the death date values
-for row in moma:
-    death_date = row[4]
-    if death_date != "":
-        death_date = int(death_date)
-    row[4] = death_date
-
-# Write your code below
-
+ages = []
 for i in moma:
     date = i[6]
-    if date != '':
-        date = int(date)
-    i[6] = date
-    
+    birth = i[3]
+    if isinstance(birth,int):
+        age = date - birth
+    else: age = 0
+    ages.append(age)
+final_ages = []
+final_age = None
+for i in ages:
+    if i > 20:
+        final_age = i
+    if i <=20:
+        final_age = "Unknown"
+    final_ages.append(final_age)
