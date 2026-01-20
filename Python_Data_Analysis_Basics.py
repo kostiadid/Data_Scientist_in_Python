50ffe0d73fa09a39ffd5f098d37fa77cf0702960
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-19 21:29:33 -0500

    date  /   death date / birth date
    date  /   death date / birth date

--- Python_Data_Analysis_Basics.py
+++ Python_Data_Analysis_Basics.py
@@ -5,3 +5,35 @@
 # Analyze and interpret the distribution of artist ages
 # Create functions that summarize our data
 # Print summaries in an easy-to-read-way
+
+
+from csv import reader
+
+# Read the `artworks_clean.csv` file
+with open('artworks_clean.csv') as opened_file:
+    read_file = reader(opened_file)
+    moma = list(read_file)
+moma = moma[1:]
+
+# Convert the birthdate values
+for row in moma:
+    birth_date = row[3]
+    if birth_date != "":
+        birth_date = int(birth_date)
+    row[3] = birth_date
+    
+# Convert the death date values
+for row in moma:
+    death_date = row[4]
+    if death_date != "":
+        death_date = int(death_date)
+    row[4] = death_date
+
+# Write your code below
+
+for i in moma:
+    date = i[6]
+    if date != '':
+        date = int(date)
+    i[6] = date
+    
