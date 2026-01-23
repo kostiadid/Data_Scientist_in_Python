d7f71fe4d0e7cdf7df03fd960262ee9a37aabec0
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-23 16:38:53 -0500

    Implement boolean indexing for January and February rides
    Extract January and February rides from pickup_month using boolean indexing.

---
+++ booleanIndexingNumPy.py
@@ -0,0 +1,12 @@
+pickup_month = taxi[:, 1]
+
+january_bool = pickup_month == 1
+january = pickup_month[january_bool]
+january_rides = january.shape[0]
+
+february_bool = pickup_month== 2
+
+february = pickup_month[february_bool]
+
+
+february_rides = february.shape[0]
