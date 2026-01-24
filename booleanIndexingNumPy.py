1eb267652580002fa6aa6b8bad050359606f961d
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-24 17:43:28 -0500

    Implement value assignment in taxi ndarray
    Add value assignment examples to taxi ndarray

--- booleanIndexingNumPy.py
+++ booleanIndexingNumPy.py
@@ -21,3 +21,24 @@ print(tip_amount)
 tip_bool = tip_amount > 20
 print(tip_bool)
 top_tips = taxi[tip_bool,5:14]
+
+
+
+#Assigning Values in Ndarrays 
+# this creates a copy of our taxi ndarray
+taxi_copy = taxi.copy()
+
+print(taxi_copy[1066,5])
+taxi_copy[1066,5] = 1
+print(taxi_copy[1066,5])
+
+print(taxi_copy[:,0])
+taxi_copy[:,0] = 16
+print(taxi_copy[:,0])
+
+print(taxi_copy[550:552,7])
+taxi_copy[550:552,7] = np.mean(taxi_copy[:,7])
+
+print(taxi_copy[550:552,7]) 
+
+rows_550_551 = taxi_copy[550:552,:]
