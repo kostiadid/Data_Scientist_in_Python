80ca58fc06a120938bc41f1b46e70759cdaea6fa
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-25 16:47:26 -0500

    Implement row selection using iloc in Data_with_Pandas.py
    Added code to select rows using iloc for specific data retrieval.

--- Data_with_Pandas.py
+++ Data_with_Pandas.py
@@ -13,19 +13,16 @@ f500.loc[f500["previous_rank"] == 0, "previous_rank"] = np.nan
 f500_selection = f500.loc[:,['rank','revenues','revenue_change']].head(5)
 
 
-
-
 #Reading CSV Files with pandas
 f500=pd.read_csv('f500.csv')
-
 f500.index.name = "Company"
 f500.columns.name = "Metric"
-
-
 bool_0_f500 = f500["previous_rank"] == 0
 f500.loc[bool_0_f500,'previous_rank'] = np.nan
 
-
+#Using iloc to Select by Integer Location
+fifth_row=f500.iloc[5]
+company_value = f500.iloc[0,0] 
 
 
 
