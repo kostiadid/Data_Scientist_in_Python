5c3bea0baab6cbdba5bcd15d5d219a767a09b541
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-25 15:44:19 -0500

    Read CSV and set DataFrame index and column names
    Added CSV reading and updated DataFrame index and column names.

--- Data_with_Pandas.py
+++ Data_with_Pandas.py
@@ -10,11 +10,20 @@ f500.index.name = None
 
 # replace 0 values in the "previous_rank" column with NaN
 f500.loc[f500["previous_rank"] == 0, "previous_rank"] = np.nan
-
-
 f500_selection = f500.loc[:,['rank','revenues','revenue_change']].head(5)
 
 
+
+
+#Reading CSV Files with pandas
+f500=pd.read_csv('f500.csv')
+
+f500.index.name = "Company"
+f500.columns.name = "Metric"
+
+
+bool_0_f500 = f500["previous_rank"] == 0
+f500.loc[bool_0_f500,'previous_rank'] = np.nan
 
 
 
