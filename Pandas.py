52e04314cf8351825d72bf7c346208236a94c1ec
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-24 20:26:33 -0500

    Implement row and item selection in DataFrame
    Added various selections and analyses of DataFrame rows and Series items.

--- Pandas.py
+++ Pandas.py
@@ -23,3 +23,47 @@ object_dtype = 6
 industries = f500['industry']
 industries_type = type(industries)
 
+#Selecting Rows from a DataFrame by Label
+revenues_years = f500[["revenues","years_on_global_500_list"]]
+countries = f500.loc[:,"country"]
+ceo_to_sector = f500.loc[:,"ceo":"sector"]
+print(countries)
+
+#Selecting Rows from a DataFrame by Label
+toyota = f500.loc['Toyota Motor']
+drink_companies = f500.loc[['Anheuser-Busch InBev','Coca-Cola','Heineken Holding']]
+middle_companies = f500.loc['Tata Motors' : 'Nationwide']
+print(toyota)
+
+#Selecting Items from a Series by Label
+countries = f500.loc[:,'country']
+country_counts = countries.value_counts()
+print(country_counts)
+top_country = 'USA'
+
+hq_locations = f500.loc[:,'hq_location']
+hql_counts = hq_locations.value_counts()
+print(hql_counts)
+
+
+#Selecting Items from a Series by Label
+countries = f500["country"]
+country_counts = countries.value_counts()
+
+india = country_counts.loc['India']
+print(india)
+
+north_america = country_counts.loc[['USA','Canada','Mexico']]
+print(north_america)
+japan_to_spain =  country_counts.loc['Japan':'Spain']
+
+#Summary Challenge
+big_movers= f500.loc[['Aviva','HP','JD.com','BHP Billiton'],['rank','previous_rank']]
+bottom_companies = f500.loc['National Grid':'AutoNation',['rank','sector','country']]
+
+revenue_giants = f500.loc[['Apple','Industrial & Commercial Bank of China','China Construction Bank','Agricultural Bank of China'],'revenues':'profit_change']
+
+
+
+
+
