653f7fad2e8e403e4f36f86fa23597e9d4e38c79
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-02-11 21:53:20 -0500

    Update Data_cleaning_basics.py

--- Data_cleaning_basics.py
+++ Data_cleaning_basics.py
@@ -10,15 +10,19 @@ for i in laptops.columns:
     
 laptops.columns = new_columns  
 
-
-
-
-
-
 gpu_split = laptops["gpu"].str.split()
 laptops["gpu_manufacturer"] = gpu_split.str[0]
 gpu_manufacturer_counts = laptops["gpu_manufacturer"].value_counts()
 
-
 laptops["cpu_manufacturer"] =laptops["cpu"].str.split().str[0]
 cpu_manufacturer_counts =  laptops["cpu_manufacturer"].value_counts()
+
+
+# Dictionary and map for replasing incorect data 
+os=laptops['os'].unique()
+
+mapping_dict = {'Android': 'Android', 'Chrome OS': 'Chrome OS','Linux': 'Linux','Mac OS': 'macOS',  'No OS': 'No OS','Windows': 'Windows',  'macOS': 'macOS'}
+laptops['os']= laptops['os'].map(mapping_dict)
+
+osCounts = laptops["os"].value_counts()
+
