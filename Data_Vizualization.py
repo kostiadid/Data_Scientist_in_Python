dc8a8378f7368deddaf6164f75cc599e9d9671a1
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-02-16 10:51:45 -0500

    Update Data_Vizualization.py

--- Data_Vizualization.py
+++ Data_Vizualization.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 who_time_series = pd.read_csv('WHO_time_series.csv')
 who_time_series = pd.DataFrame(who_time_series)
@@ -6,3 +7,17 @@ who_time_series['Date_reported'] =pd.to_datetime(who_time_series['Date_reported'
 print(who_time_series.head())
 
 print(pd.DataFrame(who_time_series).info())
+
+
+
+
+
+
+argentina = who_time_series[who_time_series['Country']=='Argentina']
+plt.plot(argentina['Date_reported'],argentina['Cumulative_cases'])
+plt.title('Argentina: Cumulative Reported Cases')
+plt.xlabel('Date')
+plt.ylabel('Number of Cases')
+
+plt.show()
+argentina_graph_type = 'exponential'
