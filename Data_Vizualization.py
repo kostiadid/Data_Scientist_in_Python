f392643fad29c6a17dbf99b3c4f2a16a952720d8
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-02-15 22:10:00 -0500

    Create Data_Vizualization.py

---
+++ Data_Vizualization.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+month_number = [1, 2, 3, 4, 5, 6, 7]
+new_deaths = [213, 2729, 37718, 184064, 143119, 136073, 165003]
+
+
+plt.plot(month_number, new_deaths)
+plt.ticklabel_format(axis='y', style='plain')
+plt.title('New Reported Deaths By Month (Globally)')
+plt.xlabel('Month Number')
+plt.ylabel('Number Of Deaths')
+plt.show()
