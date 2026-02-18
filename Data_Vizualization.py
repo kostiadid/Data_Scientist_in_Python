4ba5162d37a0125efdef1e18b6d9600d3a2b7da8
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-02-17 21:18:52 -0500

    Update Data_Vizualization.py

--- Data_Vizualization.py
+++ Data_Vizualization.py
@@ -126,5 +126,13 @@ plt.scatter(bike_sharing['workingday'], bike_sharing['registered'])
 plt.title('Working Day Vs. Registered')
 plt.show()
 
+#simple bar
+
+working_days = ['Non-Working Day', 'Working Day']
+registered_avg = [2959,3978]
+
+plt.bar(working_days, registered_avg)
+plt.show()
+
 
 
