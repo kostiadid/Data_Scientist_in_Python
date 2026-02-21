297c90986d6ca8b26e1d2e20d295801f11169544
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-02-21 15:35:30 -0500

    Update data_vizualization_pandas.py

--- data_vizualization_pandas.py
+++ data_vizualization_pandas.py
@@ -1,1 +1,10 @@
 
+import pandas as pd
+import matplotlib.pyplot as plot
+
+traffic = pd.read_csv('traffic_sao_paulo.csv', sep=';')
+traffic['Slowness in traffic (%)'] = traffic['Slowness in traffic (%)'].str.replace(',', '.')
+traffic['Slowness in traffic (%)'] = traffic['Slowness in traffic (%)'].astype(float)
+
+plot.hist(traffic['Slowness in traffic (%)'])
+plot.show()
