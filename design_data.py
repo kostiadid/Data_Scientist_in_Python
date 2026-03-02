803fb12bf94f7fd69de459583a35a26e2701b968
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-03-01 20:33:40 -0500

    ax.text(
    title and subtitle

--- design_data.py
+++ design_data.py
@@ -6,7 +6,7 @@ top20_deathtoll = pd.read_csv('top20_deathtoll.csv')
 fig, ax = plt.subplots(figsize=(4.5, 6))
 ax.barh(top20_deathtoll['Country_Other'],
         top20_deathtoll['Total_Deaths'],
-        height=0.45,color='#af0b1e')
+        height=0.45, color='#af0b1e')
 
 for location in ['left', 'right', 'top', 'bottom']:
     ax.spines[location].set_visible(False)
@@ -14,5 +14,10 @@ for location in ['left', 'right', 'top', 'bottom']:
 ax.set_xticks([0, 150000, 300000])
 ax.xaxis.tick_top()
 ax.tick_params(top=False, left=False)
-ax.tick_params(axis='x',colors='grey')
+ax.tick_params(axis='x', colors='grey')
+
+ax.text(x=-80000, y=23.5, s='The Death Toll Worldwide Is 1.5M+',
+        size=17, weight='bold')
+ax.text(x=-80000, y=22.5, s='Top 20 countries by death toll (December 2020)',
+        size=12, weight='bold')
 plt.show()
