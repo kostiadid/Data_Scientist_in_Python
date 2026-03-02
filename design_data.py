58aba0c437b5c376999f09823cc85fe8d08ade67
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-03-01 20:18:59 -0500

    color red for bars
    for  bars  color red  
    color='#af0b1e'  
    
    and  for axis x   color  of  ticks  will be grey  
    ax.tick_params(axis='x',colors='grey')

--- design_data.py
+++ design_data.py
@@ -5,10 +5,14 @@ top20_deathtoll = pd.read_csv('top20_deathtoll.csv')
 
 fig, ax = plt.subplots(figsize=(4.5, 6))
 ax.barh(top20_deathtoll['Country_Other'],
-       top20_deathtoll['Total_Deaths'],height=0.45)
+        top20_deathtoll['Total_Deaths'],
+        height=0.45,color='#af0b1e')
 
 for location in ['left', 'right', 'top', 'bottom']:
     ax.spines[location].set_visible(False)
-ax.set_xticks([0,150000,300000])   
-ax.tick_params(bottom=False, left=False)
+    
+ax.set_xticks([0, 150000, 300000])
+ax.xaxis.tick_top()
+ax.tick_params(top=False, left=False)
+ax.tick_params(axis='x',colors='grey')
 plt.show()
