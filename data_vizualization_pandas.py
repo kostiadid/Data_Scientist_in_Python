fc844cbd5b4c35886495199c51e52a3b71cbfc19
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-03-02 21:03:37 -0500

    Implement storytelling data visualization for COVID-19 deaths
    Added a storytelling data visualization section that plots COVID-19 death toll data with annotations for clarity.

--- data_vizualization_pandas.py
+++ data_vizualization_pandas.py
@@ -100,5 +100,58 @@ plt.show()
 
 
 
+#Storytelling Data Visualization
+
+death_toll = pd.read_csv('covid_avg_deaths.csv')
+deaths = [2387, 126203, 227178, 295406]
+proportions = [round(death/295406, 2) for death in deaths]
+xmax_vals = [round(0.5 + proportion * 0.3, 3) for proportion in proportions]
+
+fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(6,8))
+
+for ax in axes:
+    ax.plot(death_toll['Month'], death_toll['New_deaths'],
+            color='#af0b1e', alpha=0.1)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    ax.tick_params(bottom=False, left=False) 
+    for location in ['left', 'right', 'top', 'bottom']:
+        ax.spines[location].set_visible(False)
+        
+axes[0].plot(death_toll['Month'][:3], death_toll['New_deaths'][:3],
+         color='#af0b1e', linewidth=2.5)
+axes[1].plot(death_toll['Month'][2:6], death_toll['New_deaths'][2:6],
+         color='#af0b1e', linewidth=2.5)
+axes[2].plot(death_toll['Month'][5:10], death_toll['New_deaths'][5:10],
+         color='#af0b1e', linewidth=2.5)
+axes[3].plot(death_toll['Month'][9:12], death_toll['New_deaths'][9:12],
+         color='#af0b1e', linewidth=2.5)
+
+axes[0].text(0.5, -80, '0', alpha=0.5)
+axes[0].text(3.5, 2000, '1,844', alpha=0.5)
+axes[0].text(11.5, 2400, '2,247', alpha=0.5)
+
+axes[0].text(1.1, -300, 'Jan - Mar', color='#af0b1e',
+         weight='bold', rotation=3)
+axes[1].text(3.7, 800, 'Mar - Jun', color='#af0b1e', weight='bold')
+axes[2].text(7.1, 500, 'Jun - Oct', color='#af0b1e', weight='bold')
+axes[3].text(10.5, 600, 'Oct - Dec', color='#af0b1e', weight='bold',
+        rotation=45)
+
+axes[0].text(0.5, 3500, 'The virus kills 851 people each day',
+         size=14, weight='bold')
+axes[0].text(0.5, 3150, 'Average number of daily deaths per month in the US',
+         size=12) 
+
+for ax, xmax, death in zip(axes, xmax_vals, deaths):
+    ax.axhline(y=1600, xmin=0.5, xmax=0.8,
+               linewidth=6, color='#af0b1e',
+               alpha=0.1)
+    ax.axhline(y=1600, xmin=0.5, xmax=xmax,
+               linewidth=6, color='#af0b1e')
+    ax.text(7.5, 1850, format(death, ','),color='#af0b1e', weight='bold')
+plt.show()
+
+
 
 
